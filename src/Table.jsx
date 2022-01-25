92d5f0782eea075e04efa41b0d0afc8601a52f9a
TaChyla <tachylad.murray@gmail.com>
2022-01-25 03:01:14 -0800

    used map func to render props in TableCell

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-// import Students-data from "./students-data";
+import React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,32 +8,25 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Data from "./students-data";
 
-console.log(Data); //=>{object}
-const students = Data.data;
-console.log(students);
+const studentArray = Data.data;
 
-//map keys for each student
-const getStudentName = students.map(student => (student.name));
-const getAppStatus = students.map(student => (student.application_status));
-const getPaymentId = students.map(student => (student.payment_id));
-const getDocsIds = students.map(student => (student.document_ids));
-const getCreatedAt_date = students.map(student => (student.created_at));
-
-const createData = function( name, application_status, payment_id, document_ids, created_at ){
-    return { name, application_status, payment_id, document_ids, created_at };
-};
-
-const rows = [
-  createData('Example Name', 159, 6.0, 24, 4.0),
-  createData( getStudentName, getAppStatus, getPaymentId, getDocsIds, getCreatedAt_date ),
-];
+// const rows = [];
+//  const getValues = function(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         let studentValues = (Object.values(arr[i]));
+//         //console.log(studentValues);
+//         rows.push([studentValues]);
+//     }
+//         return rows;
+// }
+// getValues(studentArray); //returns rows with student props in an array
 
 export default function BasicTable() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
-          <TableRow>
+          <TableRow >
             <TableCell>Student Name</TableCell>
             <TableCell align="right">Application Status</TableCell>
             <TableCell align="right">Payment Id</TableCell>
@@ -42,16 +34,22 @@ export default function BasicTable() {
             <TableCell align="right">Application Created</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>{rows.map((row, index) => (
-            <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row"> {row.name}</TableCell>
-              <TableCell align="right">{row.application_status}</TableCell>
-              <TableCell align="right">{row.payment_id}</TableCell>
-              <TableCell align="right">{row.document_ids}</TableCell>
-              <TableCell align="right">{row.created_at}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+
+              <TableBody>
+                  {studentArray.map((student) => (
+                    <TableRow
+                      key={student.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row"> {student.name} </TableCell>
+                      <TableCell align="right">{student.document_ids}</TableCell>
+                      <TableCell align="right">{student.application_status}</TableCell>
+                      <TableCell align="right">{student.payment_id}</TableCell>
+                      <TableCell align="right">{student.created_at}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+
       </Table>
     </TableContainer>
   );
